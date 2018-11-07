9384aafb909663caa13c677a19b8a9ee0fd9d012
Ytreg <helgifreyr95@gmail.com>
2018-11-07 20:06:11 +0000

    Updates

---
+++ 404.js
@@ -0,0 +1,16 @@
+var segmentCount = 0;
+var l = window.location;
+const nonHomeUrls = ['/snap-ninja', '/Headcount', '/comp-3', '/Centipede'];
+for (let i = 0; i < nonHomeUrls.length; i++) {
+  const urlRegex = new RegExp(nonHomeUrls[i], 'i');
+  const verifyUrl = urlRegex.test(l.pathname);
+  if (!verifyUrl) {
+    l.replace(
+      l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') +
+      l.pathname.split('/').slice(0, 1 + segmentCount).join('/') + '/?p=/' +
+      l.pathname.slice(1).split('/').slice(segmentCount).join('/').replace(/&/g, '~and~') +
+      (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
+      l.hash
+    );
+  }
+}
