f0fd1b0e0fb91d4506c0ccfa973b52edd90e2c41
Ytreg <helgifreyr95@gmail.com>
2018-11-07 15:42:37 +0000

    Updates

---
+++ precache-manifest.c937393dc73cbd87a95327563a5207d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2033b73e6a12ddf9949c",
+    "url": "/Biomyndir/static/js/runtime~main.2033b73e.js"
+  },
+  {
+    "revision": "c2e146e6c4e880c89dd1",
+    "url": "/Biomyndir/static/js/main.c2e146e6.chunk.js"
+  },
+  {
+    "revision": "36f0f566bb07268f7efc",
+    "url": "/Biomyndir/static/js/1.36f0f566.chunk.js"
+  },
+  {
+    "revision": "c2e146e6c4e880c89dd1",
+    "url": "/Biomyndir/static/css/main.2bb671c5.chunk.css"
+  },
+  {
+    "revision": "b822cb513d9fbb20b2191beb776b8736",
+    "url": "/Biomyndir/index.html"
+  }
+];
